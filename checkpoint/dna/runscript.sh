4286e82ac3c7f7987187d32552dc9cd88927c684
Hyoungwook Nam <hwnam831@gmail.com>
2024-03-21 09:44:38 -0500

    misc: dna runscript update
    Change-Id: I6ba6f16782779017465eda280c6fa5dd8875bb4b

--- checkpoint/dna/runscript.sh
+++ checkpoint/dna/runscript.sh
@@ -3,7 +3,7 @@
 export SIM_DESC=0
 
 $GEM5_ROOT/build/X86/gem5.opt \
-        --outdir=m5_mergetest \
+        --outdir=m5_merge_dna \
 	$GEM5_ROOT/configs/example/se.py \
 	--cpu-type=AtomicSimpleCPU \
 	--num-cpus=1 \
@@ -14,10 +14,9 @@ $GEM5_ROOT/build/X86/gem5.opt \
 	--l2_size=512kB --l2_assoc=16 \
 	--l3_size=1MB --l3_assoc=16 \
         --cpu-clock=2.4GHz --sys-clock=2.4GHz \
-	--mem-size=4GB --enabledramlog \
+	--mem-size=4GB \
         --dramdeviceconfigfile=$GEM5_ROOT/ext/dramsim2/DRAMSim2/ini/DDR3_micron_32M_8B_x8_sg125.ini \
         --dramsystemconfigfile=$GEM5_ROOT/ext/dramsim2/DRAMSim2/system_reg.ini \
-        --dagprotectionfile=$GEM5_ROOT/defence/docdist/defenseDAG_0W0N_10000avg.json \
 	-c "$GEM5_ROOT/sample_programs/dna/src/mrsfast" \
         -o "--search $GEM5_ROOT/sample_programs/dna/dataset/chr3_50K.fa --seq $GEM5_ROOT/sample_programs/dna/readSimulator/chr3_50K_2000.fq" \
-	--checkpoint-dir=$GEM5_ROOT/checkpoints/mrfast/ \
+	--checkpoint-dir=$GEM5_ROOT/checkpoints/dna/ \
